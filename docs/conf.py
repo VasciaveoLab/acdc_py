cedece0137be87fbd212db47690b32d90ecbe078
LucaZanella15 <92543296+LucaZanella15@users.noreply.github.com>
2025-03-27 17:32:02 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../acdc_py'))
+sys.path.insert(0, os.path.abspath('..'))
+#sys.path.insert(0, os.path.abspath('../acdc_py'))
 
 
 # -- Project information -----------------------------------------------------
